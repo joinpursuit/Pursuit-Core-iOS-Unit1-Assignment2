32ab001d80b84ee61e2b8d7bca6883b575c49952
yazzy4 <yazzy4@gmail.com>
2018-10-16 16:09:47 -0400

    2nd commit

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
+
+
+//////Hangman////
+
+//The user must be able to play against the computer
+
+//The word that the computer chooses must be random. There is a large array of words below.
+
+//The user must have a limited number of guesses
+
+//After each guess, the computer should print out the current state of the word you are guessing. (ex. A _ _ L E, for apple)
+
+//After each guess the computer should print out how many guesses you have remaining. For full credit, make it actually print out a picture somewhat similar to the link above.
+
+//The computer should tell the user if she wins, show her how many guesses it took and end the current game
+
+//The user should see the correct answer if she loses
+
+//The user should have the option to play the game again.
+
+
+//PART ONE//
+
+
+print("Enter name:")
+let userName = readLine()!
+
+print("Welcome \(userName)! Lets play hangman!")
+
+
+//Part 1-A
+
+//print("Your have, 4 guesses so choose wisely!")
+//print("Hangman imaage- plank")
+
+//print("guess your first letter")
+//let firstLetter = readLine()
+//unwrap
+// return value pass or no
+//continue -- back to word banks
+//
 
