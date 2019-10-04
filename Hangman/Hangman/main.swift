cd136316dcba31a7eece7c67a6fea4e43fb51ad8
Amy Alsaydi <amyalsaydi@Amys-MacBook-Pro.local>
2019-10-04 17:18:07 -0400

    accepts user input

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let allTheWords = ["able", "about", "account"]
+
+let word = allTheWords.randomElement() ?? "ameni"
+
+print("enter letter")
+var choice = readLine()
+
+var gappedWord = ""
+
+for char in word {
+    if choice == String(char) {
+        // print("The letter was found")
+        // here is where i have to spit the gappedWord
+        for charac in word {
+            if charac == "a"{
+                gappedWord += String(charac)
+            } else {
+                gappedWord += "_"
+            }
+            
+        }
+        print(gappedWord)
+    }
+}
 
