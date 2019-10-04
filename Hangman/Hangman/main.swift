8c5ebcb17eb1692dac603a8fd4219fd738d07fc9
Bienbenido Angeles <bangeles@Bienbenidos-MacBook-Pro.local>
2019-10-04 17:43:25 -0400

    Creadted Hangman AI selector of words from an array

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -2,11 +2,18 @@
 //  main.swift
 //  Hangman
 //
-//  Created by Alex Paul on 10/15/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
+//  Created by Bienbenido Angeles on 10/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
 //
 
 import Foundation
+//array of words to choose
+let allTheWords = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against"]
 
-print("Hello, World!")
+//AI chooses words from an array of strings
+let allTheWordsStartIndex = allTheWords.startIndex
+let allTheWordsEndIndex = allTheWords.endIndex - 1
+let computerChoice = Int.random(in: allTheWordsStartIndex...allTheWordsEndIndex)
+let computerChoiceOfWord = allTheWords[computerChoice]
 
+print(computerChoiceOfWord)
