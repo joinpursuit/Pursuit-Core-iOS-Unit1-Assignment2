83e00fa055a9610f5f06aad085905a239cbd79cf
Amy Alsaydi <amyalsaydi@Amys-MacBook-Pro.local>
2019-10-07 14:43:38 -0400

    created hidden word to returned gapped word with each letter choice

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -11,26 +11,63 @@ import Foundation
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let allTheWords = ["able", "about", "account"]
 
-let word = allTheWords.randomElement() ?? "ameni"
+let word = allTheWords.randomElement() ?? "bob"
+print(word)
 
-print("enter letter")
-var choice = readLine()
+// creating hidden word:
 
-var gappedWord = ""
-
-for char in word {
-    if choice == String(char) {
-        // print("The letter was found")
-        // here is where i have to spit the gappedWord
-        for charac in word {
-            if charac == "a"{
-                gappedWord += String(charac)
-            } else {
-                gappedWord += "_"
-            }
-            
-        }
-        print(gappedWord)
-    }
+var hiddenWord = [String]()
+for _ in 1...word.count {
+    hiddenWord.append("_")
 }
 
+print(hiddenWord) // "_____"
+
+// setting number of turns:
+
+let numTurns = word.count + 2
+
+
+for _ in 1...numTurns { // Loop for number of turns
+    
+    print("enter letter")
+    let choice = readLine() ?? "n" // User enter letter choice
+    
+    var counter = 0
+    
+    for char in word { // loops through the word
+       
+
+        if choice == String(char) { // checks if user letter is = to
+            
+           hiddenWord[counter] = choice
+           print(hiddenWord.joined(separator:""))
+            
+        }
+        counter += 1
+        // print(counter)
+
+    }
+
+}
+
+
+/*
+ Note:
+- I have to get rid of the option once its chosen by user
+- Create a maskword that is the same length of the random word
+e.g. randomWord = ameni
+     hiddenWord = _____ // see how placement of this to other places would help.....
+*/
+
+
+
+/*
+////   string into array
+var myName = "ameni"
+print(Array(myName))
+
+/////    back into array
+var array = ["1", "2", "3"]
+print(array.joined(separator:""))
+*/
