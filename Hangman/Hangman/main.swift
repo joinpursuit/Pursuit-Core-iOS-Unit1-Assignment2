74f3c7d4ac76b425dab52be691948a325e749d48
Eric Davenport <eric_dav@lilmanns-Pursuit-MacBook.local>
2019-10-09 09:54:27 -0400

    First update

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+//============================================================
+//                       Game Start
+//============================================================
 
+print("Would you like to play Hangman? Y/N")
+let gameStart = readLine()?.uppercased()
+
+var playerGuess = readLine()
+let allTheWords = ["help", "berry", "news", "guest"]
+var word = allTheWords.randomElement()
+let totalChances = 10
+var remainingChances = 10
+var usedChances = 0
+
+repeat{
+    for _ in word {
+        print("_")
+        print("""
+        There are \(_.count) letters.
+        You have \(remainingChances) remaining guesses.
+        You have used \(usedChances) guesses
+            """)
+        
+        
+        
+    }
+    print(" ")
+} while gameStart == "Y"
