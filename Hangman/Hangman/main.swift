b2734584c436911851777924d18a78ead243c5e4
Bienbenido Angeles <bangeles@Bienbenidos-MacBook-Pro.local>
2019-10-09 17:37:39 -0400

    added game restart

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -9,11 +9,57 @@
 import Foundation
 //array of words to choose
 let allTheWords = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against"]
+let hangManDrawing = [
+    """
+""", """
+""", """
+""", """
+""", """
+""", """
+"""
+]
+print("You have a total of six attempts to guess a random word that the computer chooses or else you'll loose the game")
 
-//AI chooses words from an array of strings
-let allTheWordsStartIndex = allTheWords.startIndex
-let allTheWordsEndIndex = allTheWords.endIndex - 1
-let computerChoice = Int.random(in: allTheWordsStartIndex...allTheWordsEndIndex)
-let computerChoiceOfWord = allTheWords[computerChoice]
+//AI chooses word from an array of strings
 
-print(computerChoiceOfWord)
+var gameContinueState = false
+var numOfAttempts = 6
+
+repeat {
+    
+ //   let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    var validAIWord = String()
+
+    let computerChoiceOfWord = allTheWords.randomElement()
+    if let unwrappedRandomWord = computerChoiceOfWord {
+        validAIWord = unwrappedRandomWord
+    } else {
+        print("There is no random word to choose from")
+    }
+
+    //generate the word to have _ on each char
+//    for _ in validAIWord{
+//        print("_", terminator: " ")
+//    }
+
+    let userInputGuesses = Character(readLine() ?? "Nil value is returned")
+    
+
+    //If AI loses the game print the word
+    print("Computer choose the word: \(validAIWord)")
+    
+    //game state restart
+    print("Would you like to play again?, you can only enter \"yes\" or \"no\" ")
+    
+    let userInputPlayAgain = readLine() // gets player input if they want to play again or not
+    if let validUserInputPlayAgain = userInputPlayAgain{
+        if validUserInputPlayAgain.lowercased() == "yes" {
+            gameContinueState = true
+        } else if validUserInputPlayAgain.lowercased() == "no" {
+            gameContinueState = false
+        }
+    } else {
+        print("Valid \"yes\" or \"no\" response not detected")
+    }
+} while (gameContinueState)
+
