7d2cd612469af88ec0c9e282d5e4d1907d1fb983
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-10 12:37:08 -0400

    updated first section of code

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,50 @@
 
 import Foundation
 
-print("Hello, World!")
+print("""
+💀 Welcome to HANGMAN! 💀
+I have chosen a word for you.
+You have 6 guesses to type in the correct answer.
+""")
+
+let allTheWords = ["acid", "across", "addition", "adjustment", "advertisement", "beautiful", "because", "before", "behaviour", "belief", "company", "comparison", "competition", "complete", "complex", "cheese", "development", "different", "digestion", "direction", "dirty", "discovery"]
+
+var guessMax = 6
+var bool = true
+var randomWord: [Character] = Array(allTheWords.randomElement() ?? "Input a valid response.")
+var indices = Set<Int>()
+//SELECTED WORD
+var secretWord: [Character] = Array(repeating: "_", count: randomWord.count)
+print(secretWord)
+var answer1 = readLine()?.lowercased()
+var userInput = Character(answer1 ?? "Enter a valid response.") //make sure to convert to character
+let alphabets = Set("abscedghijklmnopqrstuvwxyz")
+//to compare answer
+for (index, char) in randomWord.enumerated(){
+    if userInput == char {
+        indices.insert(index)
+    }
+}
+
+//for guess in guessMax {
+//    guessMax -=
+//}
+
+for (index, _) in secretWord.enumerated(){
+    if indices.contains(index){
+        secretWord[index] = userInput
+    }
+}
+print(String(secretWord))
+    
+    
+//this is in the repeat statement
+
+
+// this is for user ans comparison
+//for char in allTheWords{
+//    if char.contains(){
+//        print(char)
+//    }
+//}
 
