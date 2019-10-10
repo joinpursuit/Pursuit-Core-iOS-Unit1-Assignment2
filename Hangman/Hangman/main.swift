49dfcd3f26f5ae31ddf357dda58501577431c5f6
Ian Bailey <iankbailey2@gmail.com>
2019-10-10 11:39:19 -0400

    Started

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+
+var chances = 6
+let allTheWords = ["apple", "house"]
+var randomWord = allTheWords.randomElement()
+var hiddenWord = Array(repeating: "_", count: randomWord?.count ?? 1)
+var indices = [Int]()
+var gameState = true
+
+
+
+print("Welcome to Hangman")
+sleep(1)
+print("Enter a character and guess the word, You have 6 guesses")
+
+gameLoop: while gameState == true {
+    for (index, char) in hiddenWord.enumerated()  {
+        let enteredChar = readLine()?.lowercased() ?? "z"
+        if enteredChar == char {
+            indices.append(index)
+            
+        }
+        for (index, _) in hiddenWord.enumerated() {
+            if indices.contains(index) {
+                hiddenWord[index] = enteredChar
+                 
+            }
+            }
+        print(String(hiddenWord.description))
+    }
+    
+    
+    
+    gameState = false
+}
+
+print(String(repeating: "_", count: randomWord?.count ?? 1))
+
 
