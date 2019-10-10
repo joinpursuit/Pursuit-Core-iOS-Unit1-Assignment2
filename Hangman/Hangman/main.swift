b57b042d205cbd7cf65b9af3530313f557c08920
World Domination a Brand <worlddomination@Worlds-MacBook-Pro.local>
2019-10-10 12:29:23 -0400

    more clear code written with explaination for each line

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,12 +8,15 @@
 
 import Foundation
 
+import Foundation
+
 let allTheWords = [
     
     "able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 // option command left
 // to unfold is in editor
 
+    // Instructions
 print("WELCOME tooo Hangman, a game that is much more tedious and stressfull than you middle school spelling bee because one wrong move and someone's gonna end up dead(figruatively of course).")
 print()
 
@@ -23,14 +26,61 @@ print()
 
 print("Your word has been chosen... Now it time for you to guess the word.. good luck.")
 print()
-print("please enter in a letter you would like to guess")
 
+    //This is the word that the computer chose.
 var randomWord = allTheWords.randomElement() ?? ""
+    //This counts the lenght of the word
 var randomWordCount = randomWord.count
 
+    //prints the word so I can see it and I can remember will delete before final submission
 print(randomWord)
 
-var amountOflives = 6
+    // is the place holder for the amount of letters in a word
+var placeHolder = "_ "
+    // this equates amount of letters in the chosen word to the amount of place holders
+if randomWordCount != placeHolder.count {
+    //This makes the place holder repeat itself to the amount that of word count
+let amountOfPlaceHolder = String(repeating: "_ ", count: randomWordCount)
+    print(amountOfPlaceHolder,terminator:"")
+}
+    // This prints the place holders in one line
+
+    // Establishes the variable for the amount of guesses the user has.
+var MaxGuesses = 6
+
+print()
+print()
+
+print("please enter in a letter you would like to guess")
+print()
+
+//var word = ""
+//word = randomWord
+//The user enters a letter for the random word
+var userLetter = readLine() ?? ""
+//var promptWord = "?"
+
+// create a repeat while loop that allows for the computer to check if the letter inputed is correct
+
+
+
+
+//for letter in word {
+//    let strLetter = String(letter)
+//
+//    if usedLetters.contains(strLetter) {
+//        promptWord += strLetter
+//    } else {
+//        promptWord += "?"
+//    }
+//}
+//print("The word has .. amount of letter")
+
+//for (index,char) in randomWord.enumerated() {
+//    if userLetter = char {
+//       indicies.append(index)
+//}
+//}
 
 //var randomWord = Array(repeating: "_", count: randomWordCount)
 
@@ -63,39 +113,8 @@ var amountOflives = 6
  }while(guessMax > 0)
  */
 // keep a index that tracks the word
-print()
 
 
-//
-//print()
-
-
-
-var word = ""
-word = randomWord
-var usedLetters = readLine() ?? ""
-var promptWord = "?"
-
-
-var placeHolder = "_ "
-if randomWordCount != placeHolder.count {
-let amountOfPlaceHolder = String(repeating: "_ ", count: randomWordCount)
-    print(amountOfPlaceHolder)
-}
-print(placeHolder,terminator:"")
-
-//
-//for letter in word {
-//    let strLetter = String(letter)
-//
-//    if usedLetters.contains(strLetter) {
-//        promptWord += strLetter
-//    } else {
-//        promptWord += "?"
-//    }
-//}
-//print("The word has .. amount of letter")
-//
 
 
 
