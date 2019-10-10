d8827ec86211483a465187df70f0a0e921097b91
Ian Bailey <iankbailey2@gmail.com>
2019-10-10 14:54:20 -0400

    accepts and places characters

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -10,10 +10,10 @@ import Foundation
 
 
 var chances = 6
-let allTheWords = ["apple", "house"]
-var randomWord = allTheWords.randomElement()
-var hiddenWord = Array(repeating: "_", count: randomWord?.count ?? 1)
-var indices = [Int]()
+let allTheWords = ["apple"]
+let alphabet: Array<Character> = ["a","b", "c", "d","e", "f", "g" ,"h", "i" , "j", "k","l" ,"m", "n", "o", "p", "q", "r", "s", "t", "u" , "v" , "w", "x", "y", "z" ]
+var randomWord = allTheWords.randomElement() ?? "able"
+var hiddenWord: Array<Character> = Array(repeating: "_", count: randomWord.count )
 var gameState = true
 
 
@@ -21,28 +21,36 @@ var gameState = true
 print("Welcome to Hangman")
 sleep(1)
 print("Enter a character and guess the word, You have 6 guesses")
-
 gameLoop: while gameState == true {
-    for (index, char) in hiddenWord.enumerated()  {
-        let enteredChar = readLine()?.lowercased() ?? "z"
-        if enteredChar == char {
-            indices.append(index)
+    var indices : Set<Int> = []
+    
+    let enteredChar = Character((readLine()?.lowercased() ?? "!"))
+    for (index, char) in randomWord.enumerated() {
+        if alphabet.contains(enteredChar) {
+            if enteredChar == char {
+                indices.insert(index)
+                
+            }
             
+        } else {
+            print("Enter a letter")
+            continue gameLoop
         }
-        for (index, _) in hiddenWord.enumerated() {
-            if indices.contains(index) {
-                hiddenWord[index] = enteredChar
-                 
-            }
-            }
-        print(String(hiddenWord.description))
+        
+     
     }
     
+    for (index, _) in hiddenWord.enumerated() {
+             if indices.contains(index) {
+                 hiddenWord[index] = enteredChar
+                 
+             }
+         }
+         print(String(hiddenWord.description))
     
     
-    gameState = false
 }
 
-print(String(repeating: "_", count: randomWord?.count ?? 1))
+
 
 
