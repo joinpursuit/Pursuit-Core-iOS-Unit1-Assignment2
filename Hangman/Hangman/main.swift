4066e37e858debe9f8f63ab32df1248c0b53eb74
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-10 16:03:23 -0400

    pushing update

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -17,34 +17,51 @@ You have 6 guesses to type in the correct answer.
 let allTheWords = ["acid", "across", "addition", "adjustment", "advertisement", "beautiful", "because", "before", "behaviour", "belief", "company", "comparison", "competition", "complete", "complex", "cheese", "development", "different", "digestion", "direction", "dirty", "discovery"]
 
 var guessMax = 6
+
+
 var bool = true
 var randomWord: [Character] = Array(allTheWords.randomElement() ?? "Input a valid response.")
 var indices = Set<Int>()
 //SELECTED WORD
 var secretWord: [Character] = Array(repeating: "_", count: randomWord.count)
 print(secretWord)
-var answer1 = readLine()?.lowercased()
-var userInput = Character(answer1 ?? "Enter a valid response.") //make sure to convert to character
 let alphabets = Set("abscedghijklmnopqrstuvwxyz")
 //to compare answer
-for (index, char) in randomWord.enumerated(){
-    if userInput == char {
-        indices.insert(index)
+print(randomWord)
+
+
+repeat {
+    
+    let answer1 = readLine()?.lowercased() ?? "a"
+    let userInput = Character(answer1) //make sure to convert to character
+    
+    for (index, char) in randomWord.enumerated(){
+        if userInput == char {
+            indices.insert(index)
+            for (index, _) in secretWord.enumerated(){
+                if indices.contains(index){
+                    secretWord[index] = userInput
+                    indices.remove(index)
+                }
+            }
+        }
     }
+    if userInput != randomWord.enumerated(){
+        guessMax -= 1
+        print(guessMax)
+        //            print("wrong")
+    }
+    print(String(secretWord))
+    print(indices)
 }
+    while bool == true
+
 
 //for guess in guessMax {
-//    guessMax -=
+//    guessMax -= 1
+//    }
 //}
 
-for (index, _) in secretWord.enumerated(){
-    if indices.contains(index){
-        secretWord[index] = userInput
-    }
-}
-print(String(secretWord))
-    
-    
 //this is in the repeat statement
 
 
