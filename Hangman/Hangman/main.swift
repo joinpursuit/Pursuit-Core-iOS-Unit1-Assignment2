991e81214d4fafd6d90eeb843330aae6c4bcffab
Bienbenido Angeles <bangeles@Bienbenidos-MacBook-Pro.local>
2019-10-09 20:04:32 -0400

    updated code to include evalutaton of whether or not the char the user entered is within the char of the word the ai chooses

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -26,40 +26,49 @@ var gameContinueState = false
 var numOfAttempts = 6
 
 repeat {
-    
- //   let alphabet = "abcdefghijklmnopqrstuvwxyz"
-    var validAIWord = String()
+    var validAiWord = String()
 
     let computerChoiceOfWord = allTheWords.randomElement()
     if let unwrappedRandomWord = computerChoiceOfWord {
-        validAIWord = unwrappedRandomWord
+        validAiWord = unwrappedRandomWord
     } else {
         print("There is no random word to choose from")
     }
+    
+ //   String(repeating: "_", count: validAiWord.count) // used to print "_" for number of characters in the word
+    
+    //places characters of AIs word in a array
+    var charsOfChosenWord:[Character] = []
+    for char in validAiWord {
+        charsOfChosenWord.append(char)
+    }
 
-    //generate the word to have _ on each char
-//    for _ in validAIWord{
-//        print("_", terminator: " ")
-//    }
-
+    //get character of what the user inputs
     let userInputGuesses = Character(readLine() ?? "Nil value is returned")
     
+    //array to keep track of what character values the user has entered
 
-    //If AI loses the game print the word
-    print("Computer choose the word: \(validAIWord)")
-    
-    //game state restart
-    print("Would you like to play again?, you can only enter \"yes\" or \"no\" ")
-    
-    let userInputPlayAgain = readLine() // gets player input if they want to play again or not
-    if let validUserInputPlayAgain = userInputPlayAgain{
-        if validUserInputPlayAgain.lowercased() == "yes" {
-            gameContinueState = true
-        } else if validUserInputPlayAgain.lowercased() == "no" {
-            gameContinueState = false
+    //Check to see if char the user choosed is within an array of characters that corresponds to the word the AI choosed
+    if charsOfChosenWord.contains(userInputGuesses) {
+        
+    } else if !charsOfChosenWord.contains(userInputGuesses){
+        numOfAttempts -= 1
+    }
+
+    //If user loses the game print the word
+    if numOfAttempts == 0 {
+        print("Computer choose the word: \(validAiWord)")
+        //game state restart
+        print("Would you like to play again?, you can only enter \"yes\" or \"no\" for now")
+        let userInputPlayAgain = readLine() // gets player input if they want to play again or not
+        if let validUserInputPlayAgain = userInputPlayAgain{
+            if validUserInputPlayAgain.lowercased() == "yes" {
+                gameContinueState = true
+            } else if validUserInputPlayAgain.lowercased() == "no" {
+                gameContinueState = false
+            }
+        } else {
+            print("Valid \"yes\" or \"no\" response not detected")
         }
-    } else {
-        print("Valid \"yes\" or \"no\" response not detected")
     }
 } while (gameContinueState)
-
