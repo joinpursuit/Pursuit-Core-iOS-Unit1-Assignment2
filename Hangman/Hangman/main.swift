6eb497e874e832218dc942071aeb4d12a0e6bc4d
Yuliia Engman <yuliiaengmanpursuit@Yuliias-MacBook-Pro.local>
2019-10-10 12:11:06 -0400

    start of the Hangman game

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,5 +8,75 @@
 
 import Foundation
 
-print("Hello, World!")
+print("""
+Hello!
+This is a HANGMAN GAME.
+The RULES are simple: you will see an emplty lines that hiding a word behind them.
+You will have six attempts to guess the entire world by chosing a letter per one attempt.
+Are you ready to play?
+Yes/No
+""")
+
+
+var yourAnswer = ""
+
+repeat {
+    yourAnswer = readLine()?.lowercased() ?? ""
+    if yourAnswer == "no" {
+        print("No problem. See you next time. Bye!")
+        break
+    } else if yourAnswer == "yes" {
+        print("GREAT!")
+    } else {
+        print("""
+I don't understand your answer. Please type "yes" if you want to play the game, or type "no" if you want exit the game.
+""")
+    }
+} while yourAnswer != "yes"
+
+if yourAnswer == "yes" {
+    print("""
+LET'S START!
+Your word to gues is:
+""")
+}
+
+let wordsStartingFromA = ["able"]
+    
+//    , "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake"]
+
+let randomWord = Array(wordsStartingFromA.randomElement() ?? "animal")
+
+var hiddenWord: [Character] = Array(repeating: "_", count: randomWord.count)
+
+print(hiddenWord)
+
+
+var guessMax = 6
+
+var enteredChar: Character//(readLine()?.lowercased() ?? "") // Here is a fatal error
+
+let alphabets: Set<Character> = Set("abcdefghijklmnopqrstuvwxyz")
+
+repeat {
+    print("Enter just 1 character from \"a-z\"")
+    enteredChar = Character(readLine()?.lowercased() ?? "")
+    var indices: Set<Int> = []
+
+    for (index, char) in randomWord.enumerated() {
+        if enteredChar == char {
+            indices.insert(index)
+        }
+        // update hiddenWord
+    }
+    
+    for (index, _) in hiddenWord.enumerated() {
+        if indices.contains(index) {
+            hiddenWord[index] = enteredChar
+        }
+    }
+    print(String(hiddenWord))
+    guessMax -= 1
+} while guessMax > 0
+
 
