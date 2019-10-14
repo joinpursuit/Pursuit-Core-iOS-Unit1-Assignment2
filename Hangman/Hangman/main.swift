dfce80eabe804897f0b0082d11378c03e7ce40ed
Maitree Bain <maitreebain@Maitrees-MBP.home>
2019-10-13 23:21:10 -0400

    made the game look pretty

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -9,22 +9,130 @@
 import Foundation
 
 print("""
+
 💀 Welcome to HANGMAN! 💀
 
 I have chosen a word for you.
 You have 6 guesses to type in the correct answer.
+
+Please, type in a letter to get started.
+
 """)
 
 let allTheWords = ["acid", "across", "addition", "adjustment", "advertisement", "beautiful", "because", "before", "behaviour", "belief", "company", "comparison", "competition", "complete", "complex", "cheese", "development", "different", "digestion", "direction", "dirty", "discovery"]
 
 var guessMax = 6
 
+let hangmanHead = ("""
+
+0000000000000
+0           0
+0           1
+0          1 1
+0           1
+0
+0
+0
+0
+0
+0
+0
+0
+
+""")
+
+let hangmanBody = ("""
+
+0000000000000
+0           0
+0           1
+0          1 1
+0           1
+0           2
+0           2
+0           2
+0
+0
+0
+0
+0
+
+""")
+
+let hangmanLeftArm = ("""
+
+0000000000000
+0           0
+0           1
+0          1 1
+0           1
+0          32
+0         3 2
+0        3  2
+0
+0
+0
+0
+0
+
+""")
+
+let hangmanRightArm = ("""
+
+0000000000000
+0           0
+0           1
+0          1 1
+0           1
+0          324
+0         3 2 4
+0        3  2  4
+0
+0
+0
+0
+0
+
+""")
+
+let hangmanLeftLeg = ("""
+
+0000000000000
+0           0
+0           1
+0          1 1
+0           1
+0          324
+0         3 2 4
+0        3  2  4
+0          5
+0         5
+0        5
+0       5
+0
+
+""")
+
+let hangmanRightLeg = ("""
+
+0000000000000
+0           0
+0           1
+0          1 1
+0           1
+0          324
+0         3 2 4
+0        3  2  4
+0          5 6
+0         5   6
+0        5     6
+0       5       6
+0
+
+""")
+
 var randomWord: [Character] = Array(allTheWords.randomElement() ?? "Input a valid response.")
-//SELECTED WORD
-var secretWord: [Character] = Array(repeating: "_", count: randomWord.count)
-print(secretWord)
-//to compare answer
-print(randomWord)
+var secretWord: [Character] = Array(repeating: "_" , count: randomWord.count)
 
 func restart (){
     print("""
@@ -44,7 +152,6 @@ func restart (){
 
 repeat {
     var indices = Set<Int>()
-//    print("Please enter a character, you have \(guessMax) guesses")
     let answer1 = readLine()?.lowercased() ?? "a"
 
     if answer1.count != 1 {
@@ -52,28 +159,22 @@ repeat {
         continue
     }
     
-    let userInput = Character(answer1) //make sure to convert to character
+    let userInput = Character(answer1)
     
-    
-    // go through the random word and keep track of indices where
-    // entered character is
     for (index, char) in randomWord.enumerated(){
         if userInput == char {
             indices.insert(index)
         }
     }
     
-    // update secret word to indices found above
     for (index, _) in secretWord.enumerated(){
         if indices.contains(index){
             secretWord[index] = userInput
         }
     }
-    
-    
-    
     if secretWord == randomWord {
         print("You win! The word was \(randomWord)")
+        print("You had \(guessMax) guesses left.")
         print("Would you like to play again? Type in 'yes' or 'no.'")
         let replayGame = readLine()?.lowercased()
         if replayGame == "yes" {
@@ -84,8 +185,21 @@ repeat {
     }
     if !randomWord.contains(userInput) {
         guessMax -= 1
-        
-print("Please enter a character, you have \(guessMax) guesses")
+print("Please enter a character, you have \(guessMax) guesses left.")
+        switch true{
+        case guessMax == 5:
+            print(hangmanHead)
+        case guessMax == 4:
+            print(hangmanBody)
+        case guessMax == 3:
+            print(hangmanLeftArm)
+        case guessMax == 2:
+            print(hangmanRightArm)
+        case guessMax == 1:
+            print(hangmanLeftLeg)
+        default:
+            print(hangmanRightLeg)
+        }
         if guessMax == 0 {
             print("""
 You LOSE!!!!! 🙁
